418ca27c49212e42f6033add42d8bf28629c8889
rsbappy <bappykha226@gmail.com>
2022-10-03 00:10:54 +0600

    update

---
+++ mysite/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourdjangoprojectname.heroku_settings') 
+application = StaticFilesHandler(get_wsgi_application())
